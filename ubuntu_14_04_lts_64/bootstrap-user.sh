59317d9bc1742353443404bd26c0ea414e85a8d8
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-01-28 14:29:15 +0100

    cannot use symlinks inside VMs...

--- ubuntu_14_04_lts_64/bootstrap-user.sh
+++ ubuntu_14_04_lts_64/bootstrap-user.sh
@@ -1,1 +1,21 @@
-../ubuntu_common/bootstrap-user.sh
+#!/usr/bin/env sh
+
+# setup as user
+
+yes | opam init
+eval `opam config env`
+
+opam sw 4.02.3
+eval `opam config env`
+
+cd ~
+mkdir workspace
+cd workspace/
+git clone https://github.com/nunchaku-inria/nunchaku.git
+cd nunchaku
+git pull
+yes | opam pin add -k git nunchaku .
+make
+
+
+
